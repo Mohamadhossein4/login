3dee76c4617a06fbf736e98746f898809a155c07
matin <matinjdvvbdcx@gmail.com>
2023-02-22 12:15:03 +0330

    pictur

--- login.php
+++ login.php
@@ -9,17 +9,30 @@
 <?php
 if (isset($_POST['submit'])) {
     $a = $_POST['name'];
-    $b= $_POST['last'];
+    $b = $_POST['last'];
     $_SESSION['name'] = $a;
     $_SESSION['lastname'] = $b;
-    header("location:/profile.php");
+
+
+    if (isset($_FILES['file'])) {
+        $nn = $_FILES['file']['name'];
+        $tmp = $_FILES['file']['tmp_name'];
+        if (move_uploaded_file($tmp, "picture/$nn")) {
+            $_SESSION['picture'] = $nn;
+        }
+
+
+        header("location: /profile.php");
+    }
 }
 ?>
-<form method="post">
+<form method="post" enctype="multipart/form-data">
     <label>نام خود را وارد کنید</label><br>
     <input type="text" name="name"><br>
     <label>نام خانوادگی خود را وارد کنید</label><br>
     <input type="text" name="last"><br>
+    <input type="file" name="file">
+
     <input type="submit" name="submit" value="run">
 </form>
 </body>
--- profile.php
+++ profile.php
@@ -1,3 +1,5 @@
 <?php session_start();
-echo $_SESSION['name'] . " " ;
+echo $_SESSION['name'] . " ";
 echo $_SESSION['lastname'];
+$c = $_SESSION['picture'];
+echo " <img src='picture/$c' width='1500px' height='1500px'>" ;
